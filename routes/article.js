5ea9d81a2619cc028cc6d35a33aae2a492b86559
xfnotes <2495674182@qq.com>
2017-08-09 23:23:57 +0800

    初步的将数据库改造成可用populate方法

--- routes/article.js
+++ routes/article.js
@@ -7,45 +7,70 @@ const mongoose = require('mongoose');
 const ArticleModel = mongoose.model('article');
 const UserModel = mongoose.model('users');
 
-let render = {};
 
 // 点击头像展示 特定作者的所有文章summary方式展示
 router.get('/author=*', check.NeedLogin, (req, res) => { // 此处路由所使用的正则表达式和js默认的方式所展现的情况似乎不同 
   const reqauthor = req.params[0]; // 从路由中获取author= 后面的参数
   // 根据作者 账号名称  查找所有的所属文章
-  ArticleModel.find({ author: reqauthor })
-    .then((articles) => {
+  // author: { type: mongoose.Schema.Types.Object, ref: 'users' }, // 待修改
+  //     title: { type: 'string' },
+  //     summary: { type: 'string' },
+  //     content: { type: 'string' }, // 内容
+  //     messages: [{ type: mongoose.Schema.Types.Object, ref: 'message' }],
+  //     date: { type: Date, default: Date.now },
+  //     pv: { type: 'number' },
+
+  UserModel
+    .findOne({ name: reqauthor })
+    .populate({ path: 'articles', select: 'title summary content' })
+    .then((recdata) => {
       try {
-        if (!articles) throw new Error('该作者未曾发表文章');
+        if (!recdata) throw new Error('该作者未曾发表文章');
       } catch (e) {
         req.flash('error', e.message);
         return res.redirect('/');
       }
-      if (articles) {
-        render = articles;
-        // 在已经确定有articles的情况下 查找该作者的icon
-        // 赋值函数1，为了下文的then调用: 将articles(也就是通过作者查到的文章数组)赋值给result这个容器_resdata = recdata;
-        UserModel.findOne({ name: reqauthor })
-          .then(recdata => new Promise( // 把通过作者账户名获得的账户信息中的icon赋值给render容器
-            (resolve) => { render.icon = recdata.icon; resolve(render); },
-          ))
-          .then(() => { res.render('author_summary', { res: render }); });
+      if (recdata) {
+        const renderdata = [];
+        let i = 0;
+        recdata.articles.forEach((data) => {                // 把获取到users数据转换成数组形式
+          renderdata[i] = {                                 // 原来是 {name：‘xx’,
+            name: reqauthor,                                //        icon: 'xxx',     
+            icon: recdata.icon,                             //        articles:[
+            profile: recdata.profile,                       //                  {title:'xx',summary:'xxx'}
+            title: data.title,                              //                  {title:'xx',summary:'xxx'}
+            summary: data.summary,                          //                 ]
+            content: data.content,                          // 现在是 renderdata = [
+          };                                                //                     {name:'xx',icon:'xx',title:'xx',summary:'xxx'}
+          i += 1;                                           //                     {name:'xx',icon:'xx',title:'xx',summary:'xxx'}
+        });                                                 //                    ]   为的是统一渲染网页的数据形式
+        res.render('author_summary', { res: renderdata });
       }
     });
 });
 
 // 根据文章存储自动生成的_id 来寻找文章  主要用于发表完文章后自动跳转到已发表文章页
 router.get('/id=*', check.NeedLogin, (req, res) => { // 此处路由所使用的正则表达式和js默认的方式所展现的情况似乎不同 
-  ArticleModel.findOne({ _id: req.params[0] })
-    .then((article) => {
+  ArticleModel
+    .findOne({ _id: req.params[0] })
+    .populate({ path: 'author', select: 'name icon profile' })
+    .then((recdata) => {
       try {
-        if (!article) throw new Error('链接文章失败：没有找到该文章');
+        if (!recdata) throw new Error('链接文章失败：没有找到该文章');
       } catch (e) {
         req.flash('error', e.message);
         return res.redirect('/');
       }
-      if (article) {
-        res.render('article', { res: article });
+      if (recdata) {
+        const renderdata = {
+          name: recdata.author.name,
+          icon: recdata.author.icon,
+          profile: recdata.author.profile,
+          title: recdata.title,
+          summary: recdata.summary,
+          content: recdata.content,
+        };
+        res.render('article', { res: renderdata });
       }
     });
 });
@@ -53,11 +78,18 @@ router.get('/id=*', check.NeedLogin, (req, res) => { // 此处路由所使用的
 // 文章发表 接受提交的文章数据并在校验后存储到数据库
 router.post('/', check.NeedLogin, (req, res) => {
   const article = {
-    author: req.session.user.name,
+    author: req.session.user._id,
     title: req.fields.title,
     summary: req.fields.summary,
     content: req.fields.content,
   };
+  //   author: { type: mongoose.Schema.Types.Object, ref: 'users' }, // 待修改
+  //   title: { type: 'string' },
+  //   summary: { type: 'string' },
+  //   content: { type: 'string' }, // 内容
+  //   messages: [{ type: mongoose.Schema.Types.Object, ref: 'message' }],
+  //   date: { type: Date, default: Date.now },
+  //   pv: { type: 'number' },
   try {
     if (!article.title) throw new Error('未填写标题');
     if (!article.summary) throw new Error('未填写摘要');
@@ -66,13 +98,21 @@ router.post('/', check.NeedLogin, (req, res) => {
     req.flash('error', error.message);
     return res.redirect('/article');
   }
-  ArticleModel.create(article).then((result) => { req.flash('success', '文章发表成功'); return res.redirect(`/article/id=${result._id}`); });
-  // ArticleModel.create(article)
-  //   .then(function (error, result) {
-  //     if (error) console.error(result + '不会是我？？？')  //如果要用这一段代码 ，就需要把这个函数中的error参数去掉，因为使用then之后，就不再有error这个参数了，具体的需要看promise
-  //     req.flash('success', '文章发表成功')
-  //     return res.redirect('/article')
-  //   })
+
+  ArticleModel
+    .create(article)
+    .then(
+      (recdata) => {
+        req.flash('success', '文章发表成功');
+        UserModel
+          // 根据当前用户也就是发表文章的用户的_id查找到该用户的document，并在其document中的articles里添加此次被发表的文章的_id；
+          // 由于一个用户可能发表多篇文档，所以用户的articles是一个数组，故用$addToSet为其更新数据
+          .update({ _id: req.session.user._id }, { $addToSet: { articles: recdata._id } })
+          .then(rec => console.log(rec));
+        return res.redirect(`/article/id=${recdata._id}`);
+      },
+    );
 });
+// UserModel.update({ _id: req.session.user._id }, { $addToSet: { articles: article._id } })
 
 module.exports = router;
--- routes/home.js
+++ routes/home.js
@@ -5,38 +5,37 @@ const mongoose = require('mongoose');
 const UserModel = require('../models/mongooseSchema').user(mongoose);
 const ArticleModel = require('../models/mongooseSchema').article(mongoose);
 
-let render = {};
 // 首页
-
 router.get('/', (req, res) => {
-  // 查找所有的文章 不做任何条件限制 ，获得所有文章后显示在网站文章首页
-  ArticleModel.find({})
-    .then((articles) => {
+  // 查找所有的 不做任何条件限制 ，获得所有文章后显示在网站文章首页
+  ArticleModel
+    .find({})
+    .populate({ path: 'author', select: 'name icon profile' })
+    .then((recdata) => {
       try {
-        if (!articles) throw new Error('至今为止还没有任何文章被发表');
+        if (!recdata) throw new Error('至今为止还没有任何文章被发表');
       } catch (e) {
         req.flash('error', e.message);
         return res.redirect('/');
       }
-      if (articles) {
+      if (recdata) {
         // 在已经确定有文章被发表的情况下，根据每篇文章的作者索取该作者的头像icon，然后添加到所返回的articles中，为文章缩略页的每篇文章提供头像
-        render = articles;
-
-        articles.forEach((article) => {
-          UserModel.findOne({ name: article.author })
-            .then(recdata => new Promise( // 把通过作者账户名获得的账户信息中的icon赋值给render容器
-              (resolve) => { render.icon = recdata.icon; resolve(render); console.log(0); },
-            ));
-          console.log(1);
+        const renderdata = [];
+        let i = 0;
+        recdata.forEach((data) => {
+          renderdata[i]={
+            name: data.author.name,
+            icon: data.author.icon,
+            profile: data.author.profile,
+            title: data.title,
+            summary: data.summary,
+            content: data.content,
+          };
+          i+=1;
         });
-        console.log(2);
-
-        // 在已经确定有articles的情况下 查找该作者的icon
-        // 赋值函数1，为了下文的then调用: 将articles(也就是通过作者查到的文章数组)赋值给result这个容器_resdata = recdata;
+        res.render('author_summary', { res: renderdata });
       }
-    }).then(()=>console.log(9))
-  console.log(render);
-  // res.render('author_summary', { res: render });
+    });
 });
 
 module.exports = router;
